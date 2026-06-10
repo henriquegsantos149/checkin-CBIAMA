1e74fbf2ff2e76c0134d4bfafe633fb33e5063f4
Henrique Gonzalez <henriquegonzalez@poli.ufrj.br>
2026-06-10 17:47:02 -0300

    feat: atualiza mensagens iniciais e as torna mais engajadoras

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const sendButton = document.getElementById('send-button');
 const APPS_SCRIPT_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbweL2D91roBYKIwRPuxDl5oMaUFrWaJhDd0twt-ze5PdOpZpgELkmAE95t0Qza6tnFuag/exec';
 
 const initialMessages = [
-    "Faaaala, tudo certo? Sou o <b>Agente Pro</b> e estou aqui para fazer o seu check-in no Congresso Brasileiro de Inteligência Artificial aplicada ao Meio Ambiente. Estamos super felizes com sua presença, este será um evento muito especial e espero que você aproveite bastante!",
-    "Antes de qualquer coisa, é importante que você saiba que a Ambiental Pro é um ecossistema do setor ambiental que transforma conhecimento em diferencial competitivo, e conexões em negócios estratégicos.",
-    "Temos 5 formações a nível de pós-graduação, e diversos cursos de extensão, todos reconhecidos pelo MEC. Nossas formações já transformaram a vida de mais de 80k alunos!"
+    "Faaaala, tudo bem por aí? 👋 Sou o <b>Agente Pro</b> e serei o responsável por fazer o seu check-in oficial no Congresso Brasileiro de Inteligência Artificial aplicada ao Meio Ambiente. Que alegria ter você com a gente!",
+    "Estamos preparando um evento inesquecível! Mas antes de pegar seus dados, deixa eu te contar rapidinho: a Ambiental Pro é muito mais que uma instituição de ensino. Somos um ecossistema do setor ambiental que transforma conhecimento em diferencial competitivo, e conexões em negócios estratégicos. 🌍",
+    "Para você ter ideia, já impactamos a carreira de mais de <b>80 mil alunos</b> através dos nossos cursos de extensão e das nossas 5 pós-graduações (todas reconhecidas pelo MEC). 🚀 Bora começar o seu check-in?"
 ];
 
 const questionSteps = [
